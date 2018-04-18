72010cd67b02ed9b1f0eb5845614c550160174e5
Tyler Boright <lupin3.ken@gmail.com>
2018-04-17 22:46:28 -0700

    adds a method to list eggs that fails when the filePath is incorrect

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-homedir"
 	"github.com/ttacon/chalk"
 	"github.com/urfave/cli"
+	"log"
 	"os"
 )
+
+func doesFileExist(path string) bool {
+	_, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
 
 func main() {
 	app := cli.NewApp()
@@ -24,5 +35,28 @@ func main() {
 		return nil
 	}
 
-	app.Run(os.Args)
+	app.Commands = []cli.Command{
+		{
+			Name:    "egg list",
+			Aliases: []string{"el"},
+			Usage:   "list all of the eggs you have in your boiled config",
+			Action: func(c *cli.Context) error {
+				homeDir, err := homedir.Dir()
+				if err != nil {
+					panic(err)
+				}
+
+				if doesFileExist(homeDir+"/.boiled/eggCarton.json") == false {
+					return fmt.Errorf(chalk.Red.Color("You currently do not have any eggs.  Add a boilerplate and run again!"))
+				}
+
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
