226f5fa957b8ad9809e7a159b493d893a7a8f545
Tyler Boright <lupin3.ken@gmail.com>
2018-04-17 21:44:51 -0700

    adds a main.go with some basic information about the cli app

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Boiled"
+	app.Version = "0.1.0"
+	app.Usage = "a tool to create and manage boilerplate applications"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Tyler Boright",
+			Email: "ru.lai.development@gmail.com",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Add a boilerplate project today!")
+		return nil
+	}
+
+	app.Run(os.Args)
+}
