5fde3c9eac23da0acd866091080585b66254eee3
Tyler Boright <lupin3.ken@gmail.com>
2018-04-22 18:08:31 -0700

    uses a copy file method found online and adds a unit test for the function

---
+++ copy_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+func TestCopyFile(t *testing.T) {
+	cases := []struct {
+		FilePath string
+		FailMsg  string
+	}{
+		{
+			"./fixtures/copy-dir/copy-file-test.txt",
+			"The CopyFileTest did not copy correctly",
+		},
+	}
+
+	for _, c := range cases {
+		expected, err := ioutil.ReadFile(c.FilePath)
+		if err != nil {
+			t.Error(err)
+		}
+
+		CopyFile(c.FilePath, "./fixtures/copy-dir/copy-file-test2.txt")
+		actual, err := ioutil.ReadFile("./fixtures/copy-dir/copy-file-test2.txt")
+		if err != nil {
+			t.Error(err)
+		}
+
+		if reflect.DeepEqual(expected, actual) == false {
+			t.Error(c.FailMsg)
+		}
+	}
+}
